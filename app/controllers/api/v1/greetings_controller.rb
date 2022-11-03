328cd51a638344fe4bb2a96b67a956268d140985
bruk19 <brukteshome21@gmail.com>
2022-11-03 16:39:10 +0300

    Add required changes according to the review

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -4,8 +4,8 @@ module Api
   module V1
     class GreetingsController < ApplicationController
       def index
-        greeting = Greeting.all.sample.message
-        render json: { status: 'SUCCESS', message: 'Loaded messages', greeting: }, status: :ok
+        @greeting = Greeting.all.sample
+        render json: @greeting
       end
     end
   end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3005/'
+    origins 'http://localhost:3000/'
 
     resource '*',
              headers: :any,
