9404b4918a128de599fc62635913d0a16960c29a
bruk19 <brukteshome21@gmail.com>
2022-11-03 11:18:44 +0300

    Add fix linters

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,10 +1,12 @@
-module Api 
-    module V1
-        class GreetingsController < ApplicationController
-            def index
-                greeting = Greeting.all.sample.message
-                render json: {status: 'SUCCESS', message: 'Loaded messages',  greeting: }, status: :ok
-            end
-        end
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class GreetingsController < ApplicationController
+      def index
+        greeting = Greeting.all.sample.message
+        render json: { status: 'SUCCESS', message: 'Loaded messages', greeting: }, status: :ok
+      end
     end
+  end
 end
